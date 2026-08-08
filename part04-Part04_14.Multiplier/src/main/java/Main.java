0bcbcfd3d786b27a08d5ad054eff3e65cee8146f
Your Name <your@email.com>
2026-08-08 23:59:46 +0200

    part 4 eex 16

--- part04-Part04_14.Multiplier/src/main/java/Main.java
+++ part04-Part04_14.Multiplier/src/main/java/Main.java
@@ -1,7 +1,6 @@
 
-public class Main {
 
-    public static void main(String[] args) {
+        
         // Test your Multiplier class here
 
         // Multiplier multiplyByThree = new Multiplier(3);
